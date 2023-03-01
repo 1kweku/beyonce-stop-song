c5a1eca169fd4bd7d684af357128f3d926a6d7ec
Kweku Adarkwa <1kweku@Kwekus-MacBook-Air-2.local>
2023-02-28 21:10:42 -0500

    timer functional, webpage just really wide

--- index.js
+++ index.js
@@ -1,15 +1,53 @@
-const time = document.getElementById("timer");
-const stop = document.getElementById("stop");
-const start = document.getElementById("start");
-const restart = document.getElementById("restart");
+const time = document.getElementById("time");
+const stopBtn = document.getElementById("stop");
+const startBtn = document.getElementById("start");
+const restartBtn = document.getElementById("restart");
 
-let sec = 0; //used let to create starting point for time
+let seconds = 70; //used let to create starting point for time
+//Define interval globally cause it's used in multiple functions
+let interval = null;
 
-const timerLogic = () => {
-  setInterval(() => {
-    time.innerHTML = "00:" + sec; //InnerHTML of time element is set to 0 and adds second, second increases by 1
-    sec++;
-  }, 1000); //interval defined
-};
+//Event Listeners
+startBtn.addEventListener("click", start);
+restartBtn.addEventListener("click", restartTimer);
+stopBtn.addEventListener("click", stopTimer);
 
-timerLogic();
+function timerLogic() {
+  seconds++;
+  let hour = Math.floor(seconds / 3600);
+  let min = Math.floor((seconds - hour * 3600) / 60);
+  let sec = seconds % 60;
+
+  if (sec < 10) {
+    sec = "0" + sec;
+  }
+  if (min < 10) {
+    min = "0" + min;
+  }
+  if (hour < 10) {
+    hour = "0" + hour;
+  }
+
+  time.innerHTML = `${hour}:${min}:${sec}`;
+}
+//If there is an interval, function will simply return. If there isn't, the value of interval is put to setInterval. It is put inside set interval so value can be changed on stop.
+function start() {
+  if (interval) {
+    return;
+  }
+
+  interval = setInterval(timerLogic, 1000);
+  //When you think about it, this is what a regular setInterval looks like. Function is just defined elsewhere.
+}
+
+//To stop interval, simply call clearInterval
+function stopTimer() {
+  clearInterval(interval);
+  interval = null;
+}
+
+function restartTimer() {
+  stop();
+  seconds = 0;
+  time.innerHTML = "00:00:00";
+}
